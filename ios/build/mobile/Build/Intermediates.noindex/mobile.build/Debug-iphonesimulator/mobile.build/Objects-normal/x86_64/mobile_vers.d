cc77da3166f3f434f860a0f8fbbf912a13bf07c5
allansteve <51958410+allansteve@users.noreply.github.com>
2019-08-12 22:37:15 -0300

    Commit TinDev App
    Commit TinDev App

---
+++ ios/build/mobile/Build/Intermediates.noindex/mobile.build/Debug-iphonesimulator/mobile.build/Objects-normal/x86_64/mobile_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/allaninacio/Projeto/Projetos-Native/DevTinder-React-Native/ios/build/mobile/Build/Intermediates.noindex/mobile.build/Debug-iphonesimulator/mobile.build/DerivedSources/mobile_vers.c
